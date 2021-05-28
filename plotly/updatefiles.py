6c48a0e52c22662565cab469ba4267f0d4456f44
platostwin <nmbogdan@alumni.stanford.edu>
2021-05-28 10:50:56 -0600

    Updated data.

--- plotly/updatefiles.py
+++ plotly/updatefiles.py
@@ -1,4 +1,8 @@
-# exec(open('cleandata.py').read())
+import time
+
+start = time.time()
+
+exec(open('cleandata.py').read())
 exec(open('timeline.py').read())
 exec(open('timelinebar.py').read())
 exec(open('waithistogram.py').read())
@@ -6,4 +10,4 @@ exec(open('waitbar.py').read())
 exec(open('splitters.py').read())
 exec(open('poolscatter.py').read())
 
-print('\nAll files updated.')
+print('\nAll files updated. Time to complete: %.2f minutes.' % ((time.time() - start)/60))
